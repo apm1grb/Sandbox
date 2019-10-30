3f8c2dc0302e685bff5ef69357f99721340979f8
Marko Apfel <marko.apfel@de.bosch.com>
2019-10-30 11:38:48 +0100

    initial commit

---
+++ src/NuGetFeeds/Demo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NuGetFeeds
+{
+    public class Demo
+    {
+        public void ConsolePrint(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ src/UpstreamFeedWithPaket/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpstreamFeedWithPaket
+{
+    public class Class1
+    {
+        public double Foobar { get; }
+        public int Foo { get; private set; }
+        public string Bar { get; private set; }
+
+        public Class1(double foobar)
+        {
+            Foobar = foobar;
+        }
+
+        public static Class1 Create(int i, string s)
+        {
+            return new Class1(1.0){Bar = "bar", Foo = 1};
+        }
+
+    }
+
+    public class Class2
+    {
+        public int I { get; }
+        public string S { get; private set; }
+
+        public Class2(int i, string s)
+        {
+            I = i;
+            S = s;
+        }
+    }
+}
